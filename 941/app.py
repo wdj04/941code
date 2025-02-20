dec017aabf1bedb2f48ac3616eada4e0f9066161
wdj04 <3366214741@qq.com>
2025-02-20 11:07:14 +0800

    Add files via upload

---
+++ 941/app.py
@@ -0,0 +1,10 @@
+# from flask import Flask, render_template
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def home():
+#     return render_template("index.html")
+
+# if __name__ == "__main__":
+#     app.run(debug=True,)  # 将 debug=True 设置为 False 在生产环境中更安全
